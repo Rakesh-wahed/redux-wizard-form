cff5a9d254015ef016d7bc61f2434a8029dbb25e
Frank Parejo <francisco.parejo.lopez@gmail.com>
2018-01-14 20:37:06 +0100

    create validate script. remove demo folder.

--- src/components/WizardSteps/WizardSteps.spec.jsx
+++ src/components/WizardSteps/WizardSteps.spec.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'raf/polyfill';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { WizardStepsComponent } from './WizardSteps';
