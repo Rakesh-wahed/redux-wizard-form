9547c4919f4f436a750bea77273bc145eae5e647
Frank Parejo <francisco.parejo.lopez@gmail.com>
2018-02-04 00:34:33 +0100

    feat(flow): implement flow types for containers.

--- src/containers/WizardForm.jsx
+++ src/containers/WizardForm.jsx
@@ -1,15 +1,16 @@
+// @flow
 import { connect } from 'react-redux';
 import { isWizardComplete, isLoaded, getData } from '../reducer';
 import { WizardFormComponent } from '../components/WizardForm/WizardForm';
 import { formOptionsSet, wizardReset } from '../actions';
 
 export const WizardForm = connect(
-  state => ({
+  (state: Object): Object => ({
     isWizardComplete: isWizardComplete(state),
     isLoaded: isLoaded(state),
     data: getData(state)
   }),
-  dispatch => ({
+  (dispatch: Dispatch): Object => ({
     wizardReset: () => dispatch(wizardReset()),
     onWizardOptionsLoad: formOptions => dispatch(formOptionsSet(formOptions))
   })
--- src/containers/WizardStep.jsx
+++ src/containers/WizardStep.jsx
@@ -1,17 +1,19 @@
+// @flow
 import { connect } from 'react-redux';
 import { nextStep, wizardStepNameAdd, wizardComplete } from '../actions';
 import { getFormOptions, getCurrentStep, getStepsSize } from '../reducer';
 import { WizardStepComponent } from '../components/WizardStep/WizardStep';
 
 export const WizardStep = connect(
-  state => ({
+  (state: Object): Object => ({
     formOptions: getFormOptions(state),
     currentStep: getCurrentStep(state),
     stepsSize: getStepsSize(state)
   }),
-  dispatch => ({
-    addStepName: stepName => dispatch(wizardStepNameAdd(stepName)),
-    onSubmit: () => dispatch(nextStep()),
-    onWizardComplete: data => dispatch(wizardComplete(data))
+  (dispatch: Dispatch): Object => ({
+    addStepName: (stepName: string): void =>
+      dispatch(wizardStepNameAdd(stepName)),
+    onSubmit: (): void => dispatch(nextStep()),
+    onWizardComplete: (data: Object): void => dispatch(wizardComplete(data))
   })
 )(WizardStepComponent);
